9b672c7431998e1a467cd58fe5275bd34da051d8
BERNARDO MARÇAL COSTA <bernardo_costa2@estudante.sesisenai.org.br>
2026-03-03 12:36:41 -0300

    mensagem do commit

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import Fastify from 'fastify'
+import { Pool } from 'pg'
 
+const sql = new Pool({
+    user: "postgres",
+    password: "senai",
+    host: "localhost",
+    port: 5432,
+    database: "receitas"
+})
 const servidor = Fastify();
 
 servidor.get('/usuarios', () => {
     return 'Funcionando!'
+})
+
+
+servidor.post('/usuarios', async (request, reply) => {
+    const body = request.body;
+
+    const resultado = await sql.query('select * from usuarios')
+
+    return resultado.rows
 })
 
 servidor.listen({
